36c8c246fa5771dce1a8a9ac76901544acd2c372
Romaric Drigon <romaric.drigon@gmail.com>
2012-02-01 16:03:40 +0100

    Petites modifications sur le code, les commentaires

--- Appli/src/Main.java
+++ Appli/src/Main.java
@@ -29,18 +29,18 @@ public class Main
      */
     public static void main(String[] args)
     {
-        //Ces 2 lignes permettent de préparer la BD en "dur"
+        // Ces 2 lignes permettent de préparer la BD en "dur"
         PrepareBD serviceBD = new PrepareBD();
         serviceBD.populate();        
         
         Service service = new Service();
 
-//Test création de clients avec référent minimum
+        // Test création de clients avec référent minimum
         
-        //Préparation de la date
+        // Préparation de la date
         GregorianCalendar calendar = new GregorianCalendar(1991, 10, 21);
         
-        //Préparation des listes de médiums
+        // Préparation des listes de médiums
         List<Medium> mediums = service.getAllMediums();
         
         List<Medium> listeMediums1 = new ArrayList<Medium>();
@@ -51,30 +51,32 @@ public class Main
         listeMediums1.add(mediums.get(2));
         listeMediums1.add(mediums.get(3));
 
-        //Création des clients
+        // Création des clients
         String nom = Saisie.lireChaine("Entrez le nom du client : "); 
         String prenom = Saisie.lireChaine("Entrez le prenom du client : "); 
         String adresse = Saisie.lireChaine("Entrez l'adresse : "); 
         String email = Saisie.lireChaine("Entrez l'adresse email : "); 
         String tel = Saisie.lireChaine("Entrez le numéro de téléphone : ");
-        if(service.createClient(nom, prenom, adresse, email, tel, calendar, mediums))
+        
+        if (service.createClient(nom, prenom, adresse, email, tel, calendar, mediums))
         {
             System.out.println("Création Ok");
         }
 
-        if(service.createClient("Petit", "Benoit", "avenue Gaillard", "jean@titi.com", "9876543210", calendar, listeMediums1))
+        if (service.createClient("Petit", "Benoit", "avenue Gaillard", "jean@titi.com", "9876543210", calendar, listeMediums1))
         {
             System.out.println("Création Ok");
         }
         
-        if(service.createClient("Petit2", "Benoit", "avenue Gaillard", "jean@titi.com", "9876543210", calendar, listeMediums2))
+        if (service.createClient("Petit2", "Benoit2", "avenue Gaillard2", "jean2@titi.com", "7777543210", calendar, listeMediums2))
         {
             System.out.println("Création Ok");
         }
         
-//Test de la fonctionnalité modification de clients
+        // Test de la fonctionnalité modification de clients
         Client client = service.retrieveClient(28);
         client.setNom("J'ai été modifié");
+        
         if (service.updateClient(client))
         {
             System.out.println(client.getNom() + " mis à jour");
@@ -85,9 +87,10 @@ public class Main
         }
         
         
-//Test de la connexion des employés
+        // Test de la connexion des employés
         service.connectEmploye(99, "titi");
         boolean verifierEmploye = service.connectEmploye(2, "hashedpasswd");
+        
         if (verifierEmploye)
         {
             System.out.println("Connexion ok");
@@ -97,8 +100,8 @@ public class Main
             System.out.println("Connexion ko");
         }
         
-//Test des horoscopes
-        //Récupération de tous les horoscopes
+        // Test des horoscopes
+        // Récupération de tous les horoscopes
         List<Prediction> amours = service.getPrediction(Service.PredictionType.AMOUR);
         List<Prediction> travaux = service.getPrediction(Service.PredictionType.TRAVAIL);
         List<Prediction> santes = service.getPrediction(Service.PredictionType.SANTE);
@@ -107,13 +110,14 @@ public class Main
 
         clients = service.getAllClients();
         client = clients.get(0);
+        
         System.out.println("Création d'un horoscope pour :"+client.getNom()+" "+client.getPrenom());
 
         service.createHoroscope((AmourPrediction) amours.get(0), (TravailPrediction) travaux.get(0), (SantePrediction) santes.get(0), client.getMediumsFavoris().get(0), client);
-        System.out.println(client.getHoroscope().get(0).toString());
-
         
-//Test suppression client
+        System.out.println(client.getHoroscope().get(0).toString());
+        
+        // Test suppression client
         service.deleteClient(service.retrieveClient(29));
     }
 
